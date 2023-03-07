f52f3bf3bba7789cfb57326ccdc8a0009b7f7618
Brian Westphal <brian.westphal@panchaininc.com>
2023-03-07 08:25:14 +0800

    v1.1.2: - Fixed type exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,4 @@
 export * from './config/exports';
 export * from './register-http-api-handler/exports';
+export * from './types/exports';
 export type { Express, NextFunction, Request, RequestHandler, Response } from 'express';
